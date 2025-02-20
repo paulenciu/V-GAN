bug fix in bandwith calculation

src/main.py
@@ -30,14 +30,13 @@ if __name__ == '__main__':
     torch.cuda.empty_cache()
 
     lr = 0.001
-    epochs = 1000
     latent_size= 128
 
     #res18_encoder = ResNet18AutoEncoder().get_encoder()
     #res50_encoder = ResNet50AutoEncoder().get_encoder()
 
+    epochs = 1000
     batch_size = 200
-
 
     launch_outlier_detection_experiments(
         encoder=IdentityEncoder(),
@@ -50,15 +49,15 @@ if __name__ == '__main__':
         lr=lr,
         skip_od=False,
         batch_size=batch_size,
-        momentum=0.8,
+        momentum=0,
         weight_decay=0.1,
         seed=333,
         base_estimators=[LUNAR()],
         path_to_directory="../experiments/remote",
-        filename=f"exp_scheduler_epochupdate_resized_normalized_unstandardized_mmd_v4_softmax_adam_64_occ_mvtec_1D_lr={lr}_ep={epochs}_bs={batch_size}",
+        filename=f"fit_bw_adj_exp_scheduler_epochupdate_resized_normalized_unstandardized_mmd_v4_softmax_adam_32_occ_mvtec_1D_lr={lr}_ep={epochs}_bs={batch_size}",
         penalty=MMDLossNoPenalty(),
     )
-    
+
     epochs = 200
     batch_size = 1500
 
@@ -78,7 +77,7 @@ if __name__ == '__main__':
         seed=333,
         base_estimators=[LUNAR()],
         path_to_directory="../experiments/remote",
-        filename=f"exp_scheduler_epochupdate_resized_normalized_unstandardized_mmd_v4_softmax_adam_32_occ_fmnist_1D_lr={lr}_ep={epochs}_bs={batch_size}",
+        filename=f"fit_bw_adj_exp_scheduler_epochupdate_resized_normalized_unstandardized_mmd_v4_softmax_adam_32_occ_fmnist_1D_lr={lr}_ep={epochs}_bs={batch_size}",
         penalty=MMDLossNoPenalty(),
     )
 
@@ -98,7 +97,7 @@ if __name__ == '__main__':
         seed=333,
         base_estimators=[LUNAR()],
         path_to_directory="../experiments/remote",
-        filename=f"exp_scheduler_epochupdate_resized_normalized_unstandardized_mmd_v4_softmax_adam_32_occ_syn_1D_lr={lr}_ep={epochs}_bs={batch_size}",
+        filename=f"fit_bw_adj_exp_scheduler_epochupdate_resized_normalized_unstandardized_mmd_v4_softmax_adam_32_occ_syn_1D_lr={lr}_ep={epochs}_bs={batch_size}",
         penalty=MMDLossNoPenalty(),
     )
 

src/vmmd/MMDLossConstrained.py
@@ -14,11 +14,9 @@ class RBF(nn.Module):
         self.bandwidth = bandwidth
 
     def get_bandwidth(self, L2_distances):
-        if self.bandwidth is None:
-            n_samples = L2_distances.shape[0]
-            self.bandwidth = L2_distances.data.sum() / (n_samples ** 2 - n_samples)
-            return L2_distances.data.sum() / (n_samples ** 2 - n_samples)
-        return self.bandwidth
+        n_samples = L2_distances.shape[0]
+        self.bandwidth = L2_distances.data.sum() / (n_samples ** 2 - n_samples)
+        return L2_distances.data.sum() / (n_samples ** 2 - n_samples)
 
     def forward(self, X):
         L2_distances = torch.cdist(X, X) ** 2
@@ -48,13 +46,16 @@ class MMDLossConstrained(nn.Module):
         XY = K[:X_size, X_size:].mean()
         YY = K[X_size:, X_size:].mean()
 
+        print("XX: ", XX)
+        print("YY: ", YY)
+
         mmd_loss = XX - 2 * XY + YY
 
         total_loss = mmd_loss + self.penalty.get_weighted_penalty(U)
 
         return total_loss, mmd_loss
 
-class MMDLossSquaredConstrained(nn.Module):
+class MMDLossSquareRootConstrained(nn.Module):
     '''
     Constrained loss by the number of features selected
     '''

src/vmmd/VMMD.py
@@ -29,7 +29,7 @@ from src.models.Mmd_loss_constrained import RBF
 from src.models.generator.AbstractGenerator import AbstractGenerator
 from src.utils.BigUBuilder import calculate_average_u
 from src.utils.ImageFlattenerUtility import extract_and_flatten_images_dataset_3d, unflatten_images_3d
-from src.vmmd.MMDLossConstrained import MMDLossConstrained, MMDLossSquaredConstrained
+from src.vmmd.MMDLossConstrained import MMDLossConstrained, MMDLossSquareRootConstrained
 
 
 class VMMD(ABC):
@@ -109,25 +109,22 @@ class VMMD(ABC):
         x_flattened_normalized = torch.from_numpy(normalize(x_data, axis=0)).to(torch.float32)
         x_sample = torch.Tensor(pd.DataFrame(x_flattened_normalized).sample(count).to_numpy()).to(self.device)
 
-        del x_flattened_normalized, x_data
-        with torch.no_grad():
-            u_subspaces = self.sample_count_subspaces(count)
-            x_sample = x_sample.view(-1, n_channels, height, width)
-            ux_sample = self.apply_subspaces_operator(x_sample, u_subspaces)
+        u_subspaces = self.sample_count_subspaces(count)
+        x_sample = x_sample.view(-1, n_channels, height, width)
+        ux_sample = self.apply_subspaces_operator(x_sample, u_subspaces)
 
-            x_sample_embedded = self.encode(x_sample)
-            ux_sample_embedded = self.encode(ux_sample)
-
+        x_sample_embedded = self.encode(x_sample)
+        ux_sample_embedded = self.encode(ux_sample)
 
         if type(bandwidth) == float:
             bandwidth = [bandwidth]
 
-        if not hasattr(self, 'bandwidth'):
-            mmd_loss = MMDLossConstrained()
-            mmd_loss.forward(x_sample_embedded, ux_sample_embedded, u_subspaces * 1)
-            self.bandwidth = mmd_loss.bandwidth
+        mmd_loss = MMDLossConstrained()
+        mmd_loss.forward(x_sample_embedded, ux_sample_embedded, u_subspaces * 1)
+        self.bandwidth = mmd_loss.bandwidth
 
         bw = self.bandwidth.item()
+        print("Bw: ", bw)
         mmd = tts.MMDStatistic(count, count)
         _, distances = mmd(x_sample_embedded, ux_sample_embedded, alphas=[bw], ret_matrix=True)
         pval = mmd.pval(distances)
@@ -145,7 +142,7 @@ class VMMD(ABC):
     def encode(self, x):
         return self.encoder(x).view(x.shape[0], -1)
 
-    def fit_me(self, dataset: IDataset, encoder, generator: AbstractGenerator):
+    def fit_memory_efficient(self, dataset: IDataset, encoder, generator: AbstractGenerator):
         n_channels, width, height = dataset.image_shape
         assert width == height, "Error, need square input images."
 
@@ -173,7 +170,6 @@ class VMMD(ABC):
         )
         scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
         loss_function = MMDLossConstrained(penalty=self.penalty, kernel=RBF())
-
 
         snapshot_intervals = [int(i * 0.10 * epochs) for i in range(1, 11)]
 
@@ -219,8 +215,8 @@ class VMMD(ABC):
                     if sub_batch.size(1) == 1:
                         sub_batch = sub_batch.repeat(1, 3, 1, 1)
                         processed_sub = processed_sub.repeat(1, 3, 1, 1)
-                    sub_batch = F.interpolate(sub_batch, size=224, mode='bilinear', align_corners=False)
-                    processed_sub = F.interpolate(processed_sub, size=224, mode='bilinear', align_corners=False)
+                    #sub_batch = F.interpolate(sub_batch, size=224, mode='bilinear', align_corners=False)
+                    #processed_sub = F.interpolate(processed_sub, size=224, mode='bilinear', align_corners=False)
 
                     embedded_batch = self.encode(sub_batch)
                     embedded_processed = self.encode(processed_sub)
@@ -291,16 +287,8 @@ class VMMD(ABC):
         )
 
         scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
-        #scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
 
-
-        # optimizer = torch.optim.RMSprop(
-        #     self.generator.parameters(),
-        #     lr=self.lr,
-        #     weight_decay=self.weight_decay
-        # )
-
-        torch.nn.utils.clip_grad_norm_(generator.parameters(), max_norm=1.0)
+        #torch.nn.utils.clip_grad_norm_(generator.parameters(), max_norm=1.0)
 
         self.generator_optimizer = optimizer.__class__.__name__
 
@@ -354,14 +342,11 @@ class VMMD(ABC):
                     batch = batch.repeat(1, 3, 1, 1)
                     processed_batch = processed_batch.repeat(1, 3, 1, 1)
 
-                batch = F.interpolate(batch, size=(224, 224), mode='bilinear', align_corners=False)
-                processed_batch = F.interpolate(processed_batch, size=(224, 224), mode='bilinear', align_corners=False)
+                #batch = F.interpolate(batch, size=(224, 224), mode='bilinear', align_corners=False)
+                #processed_batch = F.interpolate(processed_batch, size=(224, 224), mode='bilinear', align_corners=False)
 
-                with torch.no_grad():
-                    embedded_batch = self.encode(batch).to(self.device)
-                    embedded_processed_batch = self.encode(processed_batch).to(self.device)
-
-                del batch, processed_batch
+                embedded_batch = self.encode(batch)
+                embedded_processed_batch = self.encode(processed_batch)
 
                 batch_loss, mmd_loss = loss_function(embedded_batch, embedded_processed_batch, u_mappings)
 
@@ -372,8 +357,6 @@ class VMMD(ABC):
                 generator_loss += batch_loss.item() / batch_number
                 mmd_loss_avg += mmd_loss.item() / batch_number
 
-                # Clean up intermediate variables
-                del u_mappings, processed_batch, embedded_batch, embedded_processed_batch
 
             generator.anneal_temperature()
             scheduler.step()